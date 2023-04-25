770003304a0b5b809c3863f7eb034f199f460687
Dayeong <dayeongmaeng@gmail.com>
2023-04-25 16:33:03 +0900

    feat: 로그인 처리하기 - 직접 만든 세션 적용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,8 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 
     //    @GetMapping("/")
@@ -22,8 +25,8 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/")
-    public String homeLogin(
+    //    @GetMapping("/")
+    public String homeLoginV1(
         @CookieValue(name = "memberId", required = false) Long memberId, Model model) {
 
         if (memberId == null) {
@@ -36,6 +39,22 @@ public class HomeController {
         }
 
         model.addAttribute("member", loginMember);
+
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        //로그인
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
 
         return "loginHome";
     }
